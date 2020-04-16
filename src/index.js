93d9879e064493dc2249b8f001a03003f60db56d
Stonek79 <amit-yurist@mail.ru>
2020-04-16 21:31:59 +0300

    Commit step 4 brain games

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+/* eslint-disable no-console */
+
 import readlineSync from 'readline-sync';
 
-	export const greeting = () => { 
-		console.log("Welcome to the Brain Games!");		
-		const userName = readlineSync.question('May I have your name? ');
-		console.log('Hello, ' + userName + '!'); };
+export const greeting = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+};
+
 
 export default readlineSync;
